3eaf7fb8aa3d81b0acf11da8629c1db978a60baf
Andrew Gomes <andrewgomes1328@gmail.com>
2023-10-01 14:02:47 -0300

    UP

--- src/components/HomeComponents/About/index.jsx
+++ src/components/HomeComponents/About/index.jsx
@@ -6,10 +6,10 @@ export default function About() {
       <h1>ABOUT</h1>
       <p>
         Olá! Sou o Andrew, tenho 22 anos e sou um desenvolvedor versátil com um
-        apreço pelo frontend e o coração no backend! 💻 Minhas habilidades no
+        apreço pelo frontend e o coração no backend!<br /> Minhas habilidades no
         Front-End abrangem HTML, CSS, JavaScript, React.js, Bootstrap e Tailwind
         enquanto no Back-End, meus domínios são em PHP, Java, Laravel, Spring
-        Boot, Node.js e Express.js. Possuo conhecimentos abrangentes em Docker e
+        Boot, Node.js e Express.js. <br /> Possuo conhecimentos abrangentes em Docker e
         banco de dados, e sou altamente capacitado em versionamento com o Git e
         GitHub.
       </p>
